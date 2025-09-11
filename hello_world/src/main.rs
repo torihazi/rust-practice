eaf0eee1c7dbcc41c9a36f6dfeedf99c2bec4e54
torihazi <lovebarusa19981225@icloud.com>
2025-09-11 23:14:18 +0900

    hensuu

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 1;
+    let mut b: i32 = 2;
+    println!("a: {}", a);
+    println!("b: {}", b);
+    b = 3;
+    b += 1;
+    println!("b: {}", b);
 }
