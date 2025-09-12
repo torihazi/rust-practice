94a55ca06c1befc1e9d9d76e3ad44b0eb95b204f
torihazi <lovebarusa19981225@icloud.com>
2025-09-13 07:01:20 +0900

    knock09

--- knock09/src/main.rs
+++ knock09/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // 環境変数を受け取る
+    let env = match env::var("EDITOR") {
+        Ok(env) => env,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+    println!("EDITOR: {}", env);
+
+    let envs = env::vars();
+    for (key, value) in envs {
+        println!("{}: {}", key, value);
+    }
 }
