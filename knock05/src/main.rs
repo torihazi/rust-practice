37b334201a3d5a696b3bbeb6ba4504554577a0ad
torihazi <lovebarusa19981225@icloud.com>
2025-09-12 23:43:35 +0900

    knock05

--- knock05/src/main.rs
+++ knock05/src/main.rs
@@ -1,3 +1,10 @@
+use chrono::{DateTime, Local, Utc};
+
 fn main() {
-    println!("Hello, world!");
+    let utc: DateTime<Utc> = Utc::now();
+    println!("UTC: {}", utc);
+    println!("UTC: {}", utc.format("%Y-%m-%d %H:%M:%S"));
+    let local: DateTime<Local> = Local::now();
+    println!("Local: {}", local);
+    println!("Local: {}", local.format("%Y-%m-%d %H:%M:%S"));
 }
