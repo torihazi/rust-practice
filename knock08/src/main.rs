bd414fc47b6e2018787f921bd51d10740d3638af
torihazi <lovebarusa19981225@icloud.com>
2025-09-13 06:53:39 +0900

    knock08

--- knock08/src/main.rs
+++ knock08/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    // ファイルの内容を1行ずつ出力する
+    let file = match File::open("./knock08/src/data.txt") {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Error: {}", e);
+            return;
+        }
+    };
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 }
