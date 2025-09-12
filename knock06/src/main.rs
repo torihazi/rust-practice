ab8734e4810b66a92a7c3afd9deeacad8bc18acd
torihazi <lovebarusa19981225@icloud.com>
2025-09-12 23:59:40 +0900

    knock06

--- knock06/src/main.rs
+++ knock06/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    // 文字列の入力を受け取る
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input: &str = input.trim();
+    // 文字列を反転する
+    let reversed = input.chars().rev().collect::<String>();
+    println!("{}", reversed);
+    // 文字列を大文字に変換する
+    let upper = reversed.to_uppercase();
+    println!("{}", upper);
+    // 文字列を小文字に変換する
+    let lower = upper.to_lowercase();
+    println!("{}", lower);
 }
