bdaa22dec1133cd06ea03d2811a4d8d0d2060dcf
torihazi <lovebarusa19981225@icloud.com>
2025-09-13 19:59:52 +0900

    knock14

--- knock14/src/main.rs
+++ knock14/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // JSONファイルを読み込む
+    // parseする
+    // 整形して出力する
+    let json_str = std::fs::read_to_string("./knock14/src/data.json")?;
+    let json: Vec<serde_json::Value> = serde_json::from_str(&json_str)?;
+    println!("{:?}", json);
+    println!();
+    println!("{}", serde_json::to_string_pretty(&json)?);
+    println!();
+    Ok(())
 }
