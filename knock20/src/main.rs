a23c146d40be7fdb62ea05b7d92422b254bbc22b
torihazi <lovebarusa19981225@icloud.com>
2025-09-14 08:10:33 +0900

    knock20

--- knock20/src/main.rs
+++ knock20/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde::Deserialize;
+
+// deriveはrustのコンパイラに以下のtraitを自動実装するよう命令するための記述
+#[derive(Deserialize, Debug)]
+struct ForecastResponse {
+    forecasts: Vec<Forecast>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Forecast {
+    date: String,
+    telop: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 天気予報APIを叩き、情報を取得する
+    let body: ForecastResponse =
+        reqwest::get("https://weather.tsukumijima.net/api/forecast/city/140020")
+            .await?
+            .json()
+            .await?;
+
+    // body.forecastsはVecという可変長配列、Vecはイテレータに変換可能なのでforを使える
+    for forecast in body.forecasts {
+        println!("{}: {}", forecast.date, forecast.telop);
+    }
+    Ok(())
 }
